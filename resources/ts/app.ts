42726a1d7174818b95fe0f54bc8581619004d883
shiara02 <shiara@bpoc.co.jp>
2024-02-02 17:12:30 +0800

    Added Home Controller

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -1,1 +1,7 @@
+import HomeController from "./controller/home-controller";
 
+const initializeApp = () => {
+    window.HomeControllerInstance = new HomeController();
+};
+
+window.addEventListener('load', initializeApp);
