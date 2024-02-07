c4ff2414e6f6ac84ba19e7388d13c8f16d28822a
shiara02 <shiara@bpoc.co.jp>
2024-02-07 11:42:03 +0800

    Added reusable svg

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -12,21 +12,14 @@
     <button type="button" class="l-header__button">Contact Us</button>
 
     <button id="hamburger" class="l-header__side-btn" aria-label="Toggle Sidebar">
-        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-        </svg>
+        @include('components.svg.hamburger')
     </button>
 
     <!-- Mobile Sidebar -->
     <div id="mobileSidebar" class="l-header__side-nav hidden">
         <div class="l-header__container">
             <button id="closeSidebar">
-                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12">
-                    </path>
-                </svg>
+                @include('components.svg.close')
             </button>
             <div class="l-header__items">
                 <a href="#">Home</a>
