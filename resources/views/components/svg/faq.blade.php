1cb5e1647ed8ac62eefde69ea8742cbd7ec29a01
karl-jerico <jerico.jeks@gmail.com>
2024-02-26 15:48:44 +0800

    UHA-9: Create FAQ section for home page

---
+++ resources/views/components/svg/faq.blade.php
@@ -0,0 +1,75 @@
+<div class="c-accordion" id="accordion-flush" data-accordion="collapse"
+    data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white"
+    data-inactive-classes="c-accordion__caption">
+    <h2 id="accordion-flush-heading-1">
+        <button type="button" class="c-accordion__title" data-accordion-target="#accordion-flush-body-1"
+            aria-expanded="true" aria-controls="accordion-flush-body-1">
+            <span>Who is HiPE?</span>
+            <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M9 5 5 1 1 5" />
+            </svg>
+        </button>
+    </h2>
+    <div id="accordion-flush-body-1" class="hidden" aria-labelledby="accordion-flush-heading-1">
+        <div class="c-accordion__answer">
+            <p class="c-accordion__description">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Repudiandae neque voluptas autem amet iste sapiente omnis ratione earum? Possimus, dolore!
+                Repudiandae neque voluptas autem amet iste sapiente omnis ratione earum? Possimus, dolore!</p>
+            <p class="c-accordion__caption">Check out this guide to learn how to <a
+                    href="/docs/getting-started/introduction/"
+                    class="text-blue-600 dark:text-blue-500 hover:underline">get started</a> and start
+                developing websites even faster</p>
+        </div>
+    </div>
+    <h2 id="accordion-flush-heading-2">
+        <button type="button" class="c-accordion__title" data-accordion-target="#accordion-flush-body-2"
+            aria-expanded="false" aria-controls="accordion-flush-body-2">
+            <span>What is HiPE?</span>
+            <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M9 5 5 1 1 5" />
+            </svg>
+        </button>
+    </h2>
+    <div id="accordion-flush-body-2" class="hidden" aria-labelledby="accordion-flush-heading-2">
+        <div class="c-accordion__answer">
+            <p class="c-accordion__description">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Repudiandae neque voluptas autem amet iste sapiente omnis ratione earum? Possimus, dolore!</p>
+            <p class="c-accordion__caption">Check out this guide to learn how to <a
+                    href="/docs/getting-started/introduction/"
+                    class="text-blue-600 dark:text-blue-500 hover:underline">get started</a> and start
+                developing websites even faster</p>
+        </div>
+    </div>
+    <h2 id="accordion-flush-heading-3">
+        <button type="button" class="c-accordion__title" data-accordion-target="#accordion-flush-body-3"
+            aria-expanded="false" aria-controls="accordion-flush-body-3">
+            <span>What services do you offer?</span>
+            <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M9 5 5 1 1 5" />
+            </svg>
+        </button>
+    </h2>
+    <div id="accordion-flush-body-3" class="hidden" aria-labelledby="accordion-flush-heading-3">
+        <div class="c-accordion__answer">
+            <p class="c-accordion__description">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Repudiandae neque voluptas autem amet iste sapiente omnis ratione earum? Possimus, dolore!</p>
+            <p class="c-accordion__caption">Check out this guide to learn how to <a
+                    href="/docs/getting-started/introduction/"
+                    class="text-blue-600 dark:text-blue-500 hover:underline">get started</a> and start
+                developing websites even faster</p>
+            <p class="c-accordion__description">Learn more about these technologies:</p>
+            <ul class="ps-5 text-gray-500 list-disc dark:text-gray-400">
+                <li><a href="https://flowbite.com/pro/"
+                        class="text-blue-600 dark:text-blue-500 hover:underline">Test</a></li>
+                <li><a href="https://tailwindui.com/" rel="nofollow"
+                        class="text-blue-600 dark:text-blue-500 hover:underline">Test</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
---
+++ resources/views/sections/faq/index.blade.php
@@ -0,0 +1,18 @@
+<section id="faq" class="s-faq">
+    <div class="l-wrapper l-wrapper--fluid l-wrapper--padded-y">
+        <div class="c-title ">
+            <h2>FAQ</h2>
+        </div>
+        <div class="s-faq__content">
+            @include('components.svg.faq')
+        </div>
+        <div class="s-faq__banner">
+            <div class="s-faq__banner--left">
+                <h3>Talk to an expert</h3>
+                <p>We are here to answer all your questions.</p>
+                <button class="c-buttons--simple">Contact us now</button>
+            </div>
+            <img src="/assets/images/faq-banner.webp" alt="faq-banner">
+        </div>
+    </div>
+</section>
