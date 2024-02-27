0fa08e69bd1f07d86168095d013c103afad91b62
shiara02 <shiara@bpoc.co.jp>
2024-02-27 14:02:15 +0800

    UHA-2: Fixed lighthouse errors

--- resources/views/sections/about-us/index.blade.php
+++ resources/views/sections/about-us/index.blade.php
@@ -3,20 +3,20 @@
         <div class="s-about__container">
             <div class="s-about__container-left">
                 <div class="image--bg">
-                    <img src="../../../assets/images/about/about_bg.webp" alt="about-img">
+                    <img src="{{ mix('assets/images/about/about_bg.webp') }}" alt="about-img" width="100%" height="100%">
                 </div>
             </div>
             <div class="s-about__container-right">
                 <div class="content">
-                    <h1><span class='title-word title-word1'>Who</span><span class='title-word title-word2'>
-                            we</span><span class='title-word title-word3'> are</span></h1>
-                    <h3>Experience the essence of HiPE</h3>
+                    <h1>Who we are</h1>
+                    <h2>Experience the essence of HiPE</h2>
                     <p>HiPE is your trusted partner for innovative IT solutions. Our team of skilled Filipino engineers
                         excels in web and mobile development, design, and maintenance. We're committed to bringing your
                         vision to life and fostering lasting partnerships built on trust and excellence.</p>
                 </div>
                 <div class="image">
-                    <img src="../../../assets/images/about/about_img.webp" alt="about-hipe">
+                    <img src="{{ mix('assets/images/about/about_img.webp') }}" alt="about-hipe" class="card-img"
+                        width="100%" height="100%">
                 </div>
             </div>
         </div>
